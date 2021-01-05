ed98d318b0bf9bb1edd829cc55bbbe2272c53e7f
Andrey Safonov <a.so777@live.ca>
2021-01-05 10:51:08 -0800

    Updated WebViewer to 7.2 and updated logic for swapping images

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import WebViewer from "./PDFTronWebViewer";
-import ImageEditor from "./ImageEditor";
+import React, { useState } from 'react';
+import WebViewer from './PDFTronWebViewer';
+import ImageEditor from './ImageEditor';
 import FileSaver, { saveAs } from 'file-saver';
-import Header from "./Header";
-import "./styles.css";
+import Header from './Header';
+import './styles.css';
 
 const App = () => {
   const [webviewerInstance, setWebViewerInstance] = useState(null);
   const [imageEditorInstance, setImageEditorInstance] = useState(null);
   const [pageEdit, setPageEdit] = useState(0);
 
-  const getViewerInstance = instance => {
+  const getViewerInstance = (instance) => {
     setWebViewerInstance(instance);
   };
 
-  const getImageEditorInstance = instance => {
+  const getImageEditorInstance = (instance) => {
     setImageEditorInstance(instance);
   };
 
@@ -28,44 +28,37 @@ const App = () => {
     setPageEdit(docViewer.getCurrentPage());
     const itr = await doc.getPageIterator(docViewer.getCurrentPage());
     const currPage = await itr.current();
-    const pngBuffer = await pdfdraw.exportStream(currPage, "PNG");
+    const pngBuffer = await pdfdraw.exportStream(currPage, 'PNG');
     let blob = new Blob([pngBuffer], {
-        type: 'image/png',
-      });
-    imageEditorInstance.loadImageFromFile(blob, "image.png");
+      type: 'image/png',
+    });
+    imageEditorInstance.loadImageFromFile(blob, 'image.png');
   };
 
   const saveImage = async () => {
-    const { docViewer, PDFNet } = webviewerInstance;
-    
+    const { docViewer, CoreControls } = webviewerInstance;
+
     let image = await imageEditorInstance.toDataURL();
-    const base64Data = image.replace(/^data:image\/png;base64,/, "");
-    var binary_string = window.atob(base64Data);
-    var len = binary_string.length;
-    var bytes = new Uint8Array(len);
+    const base64Data = image.replace(/^data:image\/png;base64,/, '');
+    const binary_string = window.atob(base64Data);
+    const len = binary_string.length;
+    const bytes = new Uint8Array(len);
     for (var i = 0; i < len; i++) {
-        bytes[i] = binary_string.charCodeAt(i);
+      bytes[i] = binary_string.charCodeAt(i);
     }
+    const blob = new Blob([bytes], { type: 'image/png' });
 
-    await PDFNet.initialize();
-    const doc = await docViewer.getDocument().getPDFDoc();
-    doc.initSecurityHandler();
-    doc.lock();
-    const itr = await doc.getPageIterator(pageEdit);
-    const page = await itr.current();
-    const writer = await PDFNet.ElementWriter.create();
-    const builder = await PDFNet.ElementBuilder.create();
-    writer.beginOnPage(page, PDFNet.ElementWriter.WriteMode.e_overlay);
+    const doc = docViewer.getDocument();
 
-    const img = await PDFNet.Image.createFromMemory2(doc, bytes);
-    const matrix = await PDFNet.Matrix2D.create(docViewer.getPageWidth(pageEdit), 0, 0, docViewer.getPageHeight(pageEdit), 0, 0);
-    const element = await builder.createImageFromMatrix(img, matrix);
-    writer.writePlacedElement(element);
-    writer.end();
+    const secondDoc = await CoreControls.createDocument(blob, {
+      extension: 'png',
+    });
+    const pagesToInsert = [1];
+    const pageIndexToInsert = pageEdit;
 
-    docViewer.refreshAll();
-    docViewer.updateView();
-    docViewer.getDocument().refreshTextData();
+    doc.insertPages(secondDoc, pagesToInsert, pageIndexToInsert).then(async () => {
+      await doc.removePages([pageEdit+1]);
+    });
   };
 
   return (
--- src/components/PDFTronWebViewer.js
+++ src/components/PDFTronWebViewer.js
@@ -12,15 +12,7 @@ const PDFTronWebViewer = ({getInstance}) => {
           }, viewer.current).then((instance) => {
             // Call APIs here
             getInstance(instance);
-            instance.setTheme({
-                primary: '#1e1e1e',
-                secondary: '#1e1e1e',
-                border: '#555555',
-                buttonHover: '#000',
-                buttonActive: '#000',
-                text: '#FFFFFF',
-                icon: '#FFFFFF',
-            });
+            instance.setTheme('dark');
           })
     }, []);
 
